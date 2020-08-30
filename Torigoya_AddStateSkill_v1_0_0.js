1a786c9b7dd41223c493b26048de1ec9d15a2df2
rutan <rutan@users.noreply.github.com>
2020-08-30 13:49:52 +0900

    deploy: 2863c3700d4eea9407b1cf6e9e7e03ed17d8a4ca

---
+++ Torigoya_AddStateSkill_v1_0_0.js
@@ -0,0 +1,108 @@
+/*---------------------------------------------------------------------------*
+ * Torigoya_AddStateSkill.js v.1.0.0
+ *---------------------------------------------------------------------------*
+ * 2020/08/30 13:46 (JST)
+ *---------------------------------------------------------------------------*
+ * Ruたん ( @ru_shalm )
+ * https://torigoya-plugin.rutan.dev
+ *---------------------------------------------------------------------------*/
+
+/*:
+ * @target MV
+ * @plugindesc 使用後にステート追加スキルプラグイン (v.1.0.0)
+ * @author Ruたん（ru_shalm）
+ * @license public domain
+ * @version 1.0.0
+ * @url https://raw.githubusercontent.com/rutan/torigoya-rpg-maker-plugin/gh-pages/Torigoya_AddStateSkill.js
+ * @help
+ * 使用後にステート追加スキルプラグイン (v.1.0.0)
+ *
+ * スキル使用時に使用者にステートを追加/削除できるようにします。
+ * 「相手に大ダメージを与えるが、自分は毒になる」のようなスキルが作れます。
+ *
+ * ------------------------------------------------------------
+ * ■ 設定方法
+ * ------------------------------------------------------------
+ * スキルのメモ欄に以下のように記述してください。
+ *
+ * ■ 例1：10番のステートを付与する
+ *
+ * <AddState: 10>
+ *
+ * または
+ *
+ * <ステート追加: 10>
+ *
+ * ■ 例2：10番と11番のステートを付与する（「,」区切りで指定）
+ *
+ * <AddState: 10, 11>
+ *
+ * または
+ *
+ * <ステート追加: 10, 11>
+ *
+ * ■ 例3：10番のステートを削除する
+ *
+ * <RemoveState: 10>
+ *
+ * または
+ *
+ * <ステート削除: 10>
+ */
+
+(function () {
+    'use strict';
+
+    const Torigoya = (window.Torigoya = window.Torigoya || {});
+
+    function getPluginName() {
+        const cs = document.currentScript;
+        return cs ? cs.src.split('/').pop().replace(/\.js$/, '') : 'Torigoya_AddStateSkill';
+    }
+
+    function readParameter() {
+        const parameter = PluginManager.parameters(getPluginName());
+        return {
+            version: '1.0.0',
+        };
+    }
+
+    Torigoya.AddStateSkill = {
+        name: getPluginName(),
+        parameter: readParameter(),
+    };
+
+    function readStateIdsFromMeta(str) {
+        if (!str) return [];
+        return str
+            .split(/\s*,\s*/)
+            .filter(Boolean)
+            .map((s) => parseInt(s, 10));
+    }
+
+    (() => {
+        const upstream_BattleManager_endAction = BattleManager.endAction;
+        BattleManager.endAction = function () {
+            this.torigoyaAddStateSkill_check();
+            upstream_BattleManager_endAction.apply(this);
+        };
+
+        BattleManager.torigoyaAddStateSkill_check = function () {
+            const action = this._action;
+            if (!action) return;
+            const item = this._action.item();
+            if (!item) return;
+
+            const addStates = readStateIdsFromMeta(item.meta['AddState'] || item.meta['ステート追加']);
+            const removeStates = readStateIdsFromMeta(item.meta['RemoveState'] || item.meta['ステート削除']);
+
+            if (addStates.length === 0 && removeStates.length === 0) return;
+
+            addStates.forEach((id) => this._subject.addState(id));
+            removeStates.forEach((id) => this._subject.removeState(id));
+
+            this._logWindow.displayAutoAffectedStatus(this._subject);
+            if (this._subject.isDead()) this._subject.performCollapse();
+        };
+    })();
+})();
---
+++ Torigoya_NotRemoveWeapon.js
@@ -0,0 +1,95 @@
+/*---------------------------------------------------------------------------*
+ * Torigoya_NotRemoveWeapon.js v.1.0.0
+ *---------------------------------------------------------------------------*
+ * 2020/08/30 13:46 (JST)
+ *---------------------------------------------------------------------------*
+ * Ruたん ( @ru_shalm )
+ * https://torigoya-plugin.rutan.dev
+ *---------------------------------------------------------------------------*/
+
+/*:
+ * @target MV
+ * @plugindesc 装備画面で武器は外せないようにするプラグイン (v.1.0.0)
+ * @author Ruたん（ru_shalm）
+ * @license public domain
+ * @version 1.0.0
+ * @url https://raw.githubusercontent.com/rutan/torigoya-rpg-maker-plugin/gh-pages/Torigoya_NotRemoveWeapon.js
+ * @help
+ * 装備画面で武器は外せないようにするプラグイン (v.1.0.0)
+ *
+ * 武器を付け替えはできても、武器無しにはできないようにします
+ */
+
+(function () {
+    'use strict';
+
+    const Torigoya = (window.Torigoya = window.Torigoya || {});
+
+    function getPluginName() {
+        const cs = document.currentScript;
+        return cs ? cs.src.split('/').pop().replace(/\.js$/, '') : 'Torigoya_NotRemoveWeapon';
+    }
+
+    function readParameter() {
+        const parameter = PluginManager.parameters(getPluginName());
+        return {
+            version: '1.0.0',
+        };
+    }
+
+    Torigoya.NotRemoveWeapon = {
+        name: getPluginName(),
+        parameter: readParameter(),
+    };
+
+    (() => {
+        // -------------------------------------------------------------------------
+        // Game_Actor
+
+        Game_Actor.prototype.torigoyaNotRemoveWeapon_isWeaponSlot = function (slotId) {
+            return this.equipSlots()[slotId] === 1;
+        };
+
+        // [再定義] 全部外すで外れないようにする
+        Game_Actor.prototype.clearEquipments = function () {
+            const maxSlots = this.equipSlots().length;
+            for (let i = 0; i < maxSlots; i++) {
+                if (!this.torigoyaNotRemoveWeapon_isWeaponSlot(i) && this.isEquipChangeOk(i)) {
+                    this.changeEquip(i, null);
+                }
+            }
+        };
+
+        // 今の装備品とも比較するようにする
+        const upstream_Game_Actor_bestEquipItem = Game_Actor.prototype.bestEquipItem;
+        Game_Actor.prototype.bestEquipItem = function (slotId) {
+            const bestItem = upstream_Game_Actor_bestEquipItem.apply(this, arguments),
+                nowItem = this._equips[slotId].object(),
+                bestPerformance = bestItem ? this.calcEquipItemPerformance(bestItem) : -1000,
+                nowPerformance = nowItem ? this.calcEquipItemPerformance(nowItem) : -1000;
+
+            return nowPerformance > bestPerformance ? nowItem : bestItem;
+        };
+
+        // -------------------------------------------------------------------------
+        // Window_EquipItem
+
+        // 武器のときは末尾空白を入れない
+        const upstream_Window_EquipItem_includes = Window_EquipItem.prototype.includes;
+        Window_EquipItem.prototype.includes = function (item) {
+            if (!item && this._actor && this._actor.torigoyaNotRemoveWeapon_isWeaponSlot(this._slotId)) {
+                return false;
+            }
+            return upstream_Window_EquipItem_includes.apply(this, arguments);
+        };
+
+        // 武器のときは空白を選択できない
+        const upstream_Window_EquipItem_isEnabled = Window_EquipItem.prototype.isEnabled;
+        Window_EquipItem.prototype.isEnabled = function (item) {
+            if (!item && this._actor && this._actor.torigoyaNotRemoveWeapon_isWeaponSlot(this._slotId)) {
+                return false;
+            }
+            return upstream_Window_EquipItem_isEnabled.apply(this, arguments);
+        };
+    })();
+})();
