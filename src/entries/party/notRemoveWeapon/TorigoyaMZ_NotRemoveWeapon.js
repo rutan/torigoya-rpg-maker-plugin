2863c3700d4eea9407b1cf6e9e7e03ed17d8a4ca
ru_shalm <ru_shalm@hazimu.com>
2020-08-30 13:46:19 +0900

    port Torigoya_NotRemoveWeapon

---
+++ src/entries/party/notRemoveWeapon/TorigoyaMZ_NotRemoveWeapon.js
@@ -0,0 +1,59 @@
+import { Torigoya } from '../../../common/Torigoya';
+import { getPluginName } from '../../../common/getPluginName';
+import { readParameter } from './_build/TorigoyaMZ_NotRemoveWeapon_parameter';
+
+Torigoya.NotRemoveWeapon = {
+  name: getPluginName(),
+  parameter: readParameter(),
+};
+
+(() => {
+  // -------------------------------------------------------------------------
+  // Game_Actor
+
+  Game_Actor.prototype.torigoyaNotRemoveWeapon_isWeaponSlot = function (slotId) {
+    return this.equipSlots()[slotId] === 1;
+  };
+
+  // [再定義] 全部外すで外れないようにする
+  Game_Actor.prototype.clearEquipments = function () {
+    const maxSlots = this.equipSlots().length;
+    for (let i = 0; i < maxSlots; i++) {
+      if (!this.torigoyaNotRemoveWeapon_isWeaponSlot(i) && this.isEquipChangeOk(i)) {
+        this.changeEquip(i, null);
+      }
+    }
+  };
+
+  // 今の装備品とも比較するようにする
+  const upstream_Game_Actor_bestEquipItem = Game_Actor.prototype.bestEquipItem;
+  Game_Actor.prototype.bestEquipItem = function (slotId) {
+    const bestItem = upstream_Game_Actor_bestEquipItem.apply(this, arguments),
+      nowItem = this._equips[slotId].object(),
+      bestPerformance = bestItem ? this.calcEquipItemPerformance(bestItem) : -1000,
+      nowPerformance = nowItem ? this.calcEquipItemPerformance(nowItem) : -1000;
+
+    return nowPerformance > bestPerformance ? nowItem : bestItem;
+  };
+
+  // -------------------------------------------------------------------------
+  // Window_EquipItem
+
+  // 武器のときは末尾空白を入れない
+  const upstream_Window_EquipItem_includes = Window_EquipItem.prototype.includes;
+  Window_EquipItem.prototype.includes = function (item) {
+    if (!item && this._actor && this._actor.torigoyaNotRemoveWeapon_isWeaponSlot(this._slotId)) {
+      return false;
+    }
+    return upstream_Window_EquipItem_includes.apply(this, arguments);
+  };
+
+  // 武器のときは空白を選択できない
+  const upstream_Window_EquipItem_isEnabled = Window_EquipItem.prototype.isEnabled;
+  Window_EquipItem.prototype.isEnabled = function (item) {
+    if (!item && this._actor && this._actor.torigoyaNotRemoveWeapon_isWeaponSlot(this._slotId)) {
+      return false;
+    }
+    return upstream_Window_EquipItem_isEnabled.apply(this, arguments);
+  };
+})();
