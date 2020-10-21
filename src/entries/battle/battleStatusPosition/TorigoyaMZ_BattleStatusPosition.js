016aa64855686bab29695b89174f909c1838f693
ru_shalm <ru_shalm@hazimu.com>
2020-10-22 03:19:31 +0900

    add battleStatusPosition

---
+++ src/entries/battle/battleStatusPosition/TorigoyaMZ_BattleStatusPosition.js
@@ -0,0 +1,32 @@
+import { Torigoya } from '../../../common/Torigoya';
+import { getPluginName } from '../../../common/getPluginName';
+import { readParameter } from './_build/TorigoyaMZ_BattleStatusPosition_parameter';
+
+Torigoya.BattleStatusPosition = {
+  name: getPluginName(),
+  parameter: readParameter(),
+};
+
+(() => {
+  // パーティメンバーの人数に応じて位置を変える
+  Window_BattleStatus.prototype.itemRect = function (index) {
+    const maxCols = this.maxCols();
+
+    const itemWidth = this.itemWidth();
+    const itemHeight = this.itemHeight();
+    const colSpacing = this.colSpacing();
+    const rowSpacing = this.rowSpacing();
+    const width = itemWidth - colSpacing;
+    const height = itemHeight - rowSpacing;
+
+    const itemSize = Math.min(this.maxItems(), this.maxCols());
+    const totalWidth = (this.innerWidth / this.maxCols()) * itemSize;
+
+    const col = index % maxCols;
+    const row = Math.floor(index / maxCols);
+    const x = (this.innerWidth - totalWidth) / 2 + col * itemWidth + colSpacing / 2 - this.scrollBaseX();
+    const y = row * itemHeight + rowSpacing / 2 - this.scrollBaseY();
+
+    return new Rectangle(x, y, width, height);
+  };
+})();
