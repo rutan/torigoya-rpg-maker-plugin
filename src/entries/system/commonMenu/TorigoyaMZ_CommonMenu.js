4465a0024da49afc9d0c926d188029ff5e704f32
rutan <ru_shalm@hazimu.com>
2022-04-17 02:48:52 +0900

    use PluginCommonBase.js

--- src/entries/system/commonMenu/TorigoyaMZ_CommonMenu.js
+++ src/entries/system/commonMenu/TorigoyaMZ_CommonMenu.js
@@ -8,15 +8,55 @@ Torigoya.CommonMenu = {
 };
 
 (() => {
+  /**
+   * メニュー項目の追加
+   * @param item
+   * @param index
+   */
+  Window_MenuCommand.prototype.torigoyaCommonMenu_addCommand = function (item, index) {
+    const enabled = this.torigoyaCommonMenu_isEnable(item);
+    if (!enabled && !this.torigoyaCommonMenu_isVisibility(item)) return;
+
+    this.addCommand(this.torigoyaCommonMenu_itemName(item), `TorigoyaCommonMenu_${index}`, enabled);
+  };
+
+  /**
+   * メニュー項目が有効であるか？
+   * @param item
+   * @returns {boolean}
+   */
+  Window_MenuCommand.prototype.torigoyaCommonMenu_isEnable = function (item) {
+    return item.switchId ? $gameSwitches.value(parseInt(item.switchId, 10)) : true;
+  };
+
+  /**
+   * メニュー項目が可視状態であるか？
+   * @param item
+   * @returns {boolean}
+   */
+  Window_MenuCommand.prototype.torigoyaCommonMenu_isVisibility = function (item) {
+    return item.visibility;
+  };
+
+  /**
+   * メニュー項目の名前を取得
+   * @param item
+   * @returns {string}
+   */
+  Window_MenuCommand.prototype.torigoyaCommonMenu_itemName = function (item) {
+    if (!item) return '';
+
+    if (window.PluginManagerEx && window.PluginManagerEx.convertEscapeCharacters) {
+      return window.PluginManagerEx.convertEscapeCharacters(item.name);
+    }
+    return item.name;
+  };
+
   const upstream_Window_MenuCommand_addOriginalCommands = Window_MenuCommand.prototype.addOriginalCommands;
   Window_MenuCommand.prototype.addOriginalCommands = function () {
     upstream_Window_MenuCommand_addOriginalCommands.apply(this);
 
-    Torigoya.CommonMenu.parameter.baseItems.forEach((item, i) => {
-      const enabled = item.switchId ? $gameSwitches.value(parseInt(item.switchId, 10)) : true;
-      if (!enabled && !item.visibility) return;
-      this.addCommand(item.name, `TorigoyaCommonMenu_${i}`, enabled);
-    });
+    Torigoya.CommonMenu.parameter.baseItems.forEach(this.torigoyaCommonMenu_addCommand.bind(this));
   };
 
   const upstream_Scene_Menu_createCommandWindow = Scene_Menu.prototype.createCommandWindow;
