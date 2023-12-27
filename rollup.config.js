fa4dbe93a52eb32e7d80d384cff58b2f4ecfd99d
rutan <ru_shalm@hazimu.com>
2023-12-27 17:39:34 +0900

    refactor rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,35 @@
-import * as path from 'path';
+import { basename, dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { glob } from 'glob';
 import { babel } from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import applyTemplate from './extensions/rollup/rollup-apply-template.js';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const config = glob.sync(path.join(dirname, 'src', 'entries', '*', '*', 'Torigoya*.js')).map((input) => {
+const config = glob.sync(resolve(__dirname, 'src', 'entries', '*', '*', 'Torigoya*.js')).map((input) => {
   const isMZ = input.includes('TorigoyaMZ');
 
   return {
     input,
     output: {
-      file: `_dist/${path.basename(input)}`,
+      file: `_dist/${basename(input)}`,
       format: 'iife',
     },
     plugins: [
-      resolve({
+      nodeResolve({
         browser: true,
       }),
       commonjs(),
       isMZ ? null : babel({ babelHelpers: 'bundled' }),
       replace({
-        __entryFileName: JSON.stringify(path.basename(input).replace(/\.[^\.]+$/, '')),
+        __entryFileName: JSON.stringify(basename(input).replace(/\.[^\.]+$/, '')),
         preventAssignment: false,
       }),
       applyTemplate({
-        template: path.resolve(dirname, 'src', 'templates', 'plugin.ejs'),
+        template: resolve(__dirname, 'src', 'templates', 'plugin.ejs'),
       }),
     ].filter(Boolean),
     onwarn(warning, warn) {
